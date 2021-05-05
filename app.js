eee70e52fa979cb1cb337abb968f5f7339d7d50b
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-05 16:31:59 -0700

    grabbed dom elements, initialized state, updated const, id names

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // import functions and grab DOM elements
+const submitButton = document.querySelector('submit-button');
+const gameResultsDiv = document.querySelector('game-results-div');
+const totalGamesDiv = document.querySelector('total-games-div');
+const userWinsDiv = document.querySelector('user-wins-div');
+const resetButton = document.querySelector('reset-button');
 
 // initialize state
+let totalGames = 0;
+let totalWins = 0;
+let totalLosses = 0;
+let computerThrow = Math.ceil(Math.random() * 3);
 
 // set event listeners to update state and DOM
