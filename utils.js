e0d6004049f790881ee39a9c65a7dbe73da8673b
Derek Hudgins <hudgins.derek@gmail.com>
2021-05-07 14:11:08 -0700

    refactored function

--- utils.js
+++ utils.js
@@ -8,26 +8,42 @@ export function randomThrow(computerThrow) {
 }
 
 
-export function userWin(userThrow, computerThrow) {
+// export function userWin(userThrow, computerThrow) {
+//     if ((userThrow === 'Ro' && computerThrow === 'Bo')
+//         || (userThrow === 'Sham' && computerThrow === 'Ro')
+//             || (userThrow === 'Bo' && computerThrow === 'Sham'))
+//     {
+//         return 'win';
+//     }
+// }
+
+// export function userLose(userThrow, computerThrow) {
+//     if ((userThrow === 'Bo' && computerThrow === 'Ro')
+//         || (userThrow === 'Ro' && computerThrow === 'Sham')
+//             || (userThrow === 'Sham' && computerThrow === 'Bo'))
+//     {
+//         return 'lose';
+//     } 
+// }
+
+// export function gameDraw(userThrow, computerThrow) {
+//     if ((userThrow === computerThrow)) {
+//         return 'draw';
+//     }
+// }
+
+
+
+export function gameResult(userThrow, computerThrow) {
     if ((userThrow === 'Ro' && computerThrow === 'Bo')
         || (userThrow === 'Sham' && computerThrow === 'Ro')
-            || (userThrow === 'Bo' && computerThrow === 'Sham'))
-    {
+            || (userThrow === 'Bo' && computerThrow === 'Sham')){
         return 'win';
+
     }
-}
-
-export function userLose(userThrow, computerThrow) {
-    if ((userThrow === 'Bo' && computerThrow === 'Ro')
-        || (userThrow === 'Ro' && computerThrow === 'Sham')
-            || (userThrow === 'Sham' && computerThrow === 'Bo'))
-    {
-        return 'lose';
-    } 
-}
-
-export function gameDraw(userThrow, computerThrow) {
-    if ((userThrow === computerThrow)) {
+    if (userThrow === computerThrow) {
         return 'draw';
+
     }
+    return 'lose';
 }
